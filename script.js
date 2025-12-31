6d402df48f69493f7136ad521860236b3188c6b4
Sonali Jamadar <99808291+sonalijamadar@users.noreply.github.com>
2025-12-31 14:53:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,22 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	const prices = document.querySelectorAll(".price");
+	let total = 0;
+	prices.forEach(price => {
+		total += Number(price.innerText);
+	});
+	const table = document.querySelector("table");
+	if(document.getElementById("totalRow"))
+return;
+	const totalRow = document.createElement("tr");
+	totalRow.id = "totalRow";
+	const totalCell = document.createElement("td");
+	totalCell.colSpan = 2;
+	totalCell.innerText = "Total Price: RS " + total;
+
+	totalRow.appendChild(totalCell);
+	table.appendChild(totalRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
