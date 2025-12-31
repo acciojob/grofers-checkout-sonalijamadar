ba2e0fb634f0ac436f282e4ba942b509beb03053
Sonali Jamadar <99808291+sonalijamadar@users.noreply.github.com>
2025-12-31 15:06:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-	const prices = document.querySelectorAll(".prices");
+	const prices = document.querySelectorAll(".price");
 	let total = 0;
 	prices.forEach(price => {
 		total += Number(price.innerText);
